7a07b9a1c4c339720d137ac63b8efb8cf6dda295
Jonathan Benusa <Benu9372@stthomas.edu>
2022-04-09 23:15:30 -0500

    Finally got isSumMem() to work

--- DynamicSum.java
+++ DynamicSum.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -15,8 +14,7 @@ import java.util.Arrays;
 
 public class DynamicSum{
 
-    private Boolean [][] table;
-    private int sum;
+    private BoolInt[][] table;
 
     public DynamicSum(){
         //YOUR CODE HERE
@@ -34,25 +32,31 @@ public class DynamicSum{
      */
     public boolean isSum(int[] arr, int t){
         //YOUR CODE HERE
-        Arrays.sort(arr);
-        ArrayList sums = new ArrayList<Integer>();      //sums will hold all previous calculated sums
+        int[][] sums = new int[arr.length][t+1];      //sums will hold all previous calculated sums
+        int index = 0;
 
-        for(int currentValue:arr){
-            if(currentValue == t){                      //if the current value from the array is i
+        sums[0][0] = arr[0];
+
+        for(int i=1; i<arr.length; i++) {
+            index=0;
+            if(arr[i]==t) {
                 return true;
-            } else {
-                int sumsSize = sums.size();             //the size of sums before looking at this current index
-                //add the current index's value to all previous additions and check if the summation is equal to t
-                for (int i=0; i<sums.size(); i++) {
-                    Integer a = (Integer) sums.get(i);
-                    Integer curSum = a+currentValue;
-                    if((curSum).equals(t)) {
+            }else if(arr[i]<t){
+                for(int sum:sums[i-1]) {
+                    int num=sum+arr[i];
+                    if(num==t) {
                         return true;
-                    }else {
-                        sums.add(curSum);               //if not add this sum to the list of previous sums
+                    }
+                    if(num<t && !Arrays.stream(sums[i]).anyMatch(x -> x == num)) {
+                        sums[i][index] = num;
+                        index++;
                     }
                 }
-                sums.add(currentValue);
+                int I = arr[i];
+                if(!Arrays.stream(sums[i]).anyMatch(x -> x == I)) {
+                    sums[i][index] = I;
+                    index++;
+                }
             }
         }
 
@@ -61,6 +65,9 @@ public class DynamicSum{
 
     /**
      *Checks to see if a subset of arr adds up to exactly t with a memoizied solution.
+     *
+     * This method will at worst make n recursive calls. For calls n1 to n it at worst will do t^2 work. For call
+     * n0 it will at worst run in constant time. This gives a O(n+(n-1)*t^2) which will simplify to O(n * t^2).
 	 *
      * @param arr the array of integers to take subsets from.
      * @param t   the value the subset could add up to.
@@ -69,10 +76,71 @@ public class DynamicSum{
      */
     public boolean isSumMem(int[] arr, int t ){
         //YOUR CODE HERE
-        table = new Boolean[arr.length][arr.length];
-        
-        return false;
+        int i = arr.length;
+        table = new BoolInt[i][t+1];
+        return isSumPriv(arr, t, i-1, 0);
     }//isSumMem
+    private boolean isSumPriv(int[] arr, int t, int i, int j) {
+        if(table[i][j]!=null && table[i][j].getIsTrue()==true) {        //if the table entry is true return true
+            return true;
+        }
+        if(table[i][j]!=null && table[i][j].getIsTrue()==false) {       //if the table entry is false return false
+            for(BoolInt entry: table[i]) {      //we need to check all entries at this level because one of them might be true while the rest are not
+                if(table[i][j]!=null && table[i][j].getIsTrue()==true) {
+                    return true;
+                }
+            }
+            return false;
+        } else {        //if to this point then the BoolInt is still null
+
+            if(i==0) {  //this will be the terms for setting up the first row of the table runs in constant time
+                if(arr[i]==t) {
+                    table[i][j] = new BoolInt(arr[i], true);
+                    return true;
+                }else if(arr[i]>t) {
+                    table[i][j] = new BoolInt(false);
+                    return false;
+                }else if(arr[i]<t) {
+                    table[i][j] = new BoolInt(arr[i], false);
+                    return false;
+                }
+            }
+            if(table[i-1][j]==null) {               // if this row of the table has not been filled yet
+                if(isSumPriv(arr, t, i-1, 0)==true) {       //if the previous row returns true then set this row to true and return true
+                    table[i][j] = new BoolInt(true);
+                    return true;
+                }else {
+                    //runs t times
+                    for(BoolInt entry : table[i-1]) {           //otherwise check by adding each previous sum and store the sum in the table if it is new and less than t
+                        if(entry!=null) {
+                            Boolean repeated = false;
+                            int num = entry.getNum() + arr[i];
+                            //runs t times with constant work
+                            for(BoolInt contains : table[i-1]) {
+                                if(contains != null) {
+                                    if (contains.getNum() == num) {
+                                        repeated = true;
+                                    }
+                                }
+
+                            }
+                            table[i][j] = new BoolInt(entry.getNum(), false);       //makes it so all sums that can be made with indexs 0-i are stored in row i for convienence.
+                            j++;
+                            if(!repeated && num<=t) {
+                                table[i][j] = new BoolInt(num, false);
+                                if (table[i][j].getNum() == t) {
+                                    table[i][j].setIsTrue(true);                    //if any of these new sums are equal to t then set row equal to true and return true
+                                    return true;
+                                }
+                                j++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
     /**
      * Recovers the subset of arr that adds up to t, if it exists.
@@ -88,4 +156,30 @@ public class DynamicSum{
         return null;
     }//getSubset
 
+    private class BoolInt {
+        private Integer num;
+        private Boolean isTrue;
+
+        public BoolInt(int num, boolean isTrue) {
+            this.num = num;
+            this.isTrue = isTrue;
+        }
+        public BoolInt(boolean isTrue) {
+            this.isTrue = isTrue;
+            num  = null;
+        }
+
+        public int getNum() {
+            return num;
+        }
+        public void updateInt(int num) {
+            this.num = num;
+        }
+        public void setIsTrue(boolean bool) {
+            isTrue = bool;
+        }
+        public Boolean getIsTrue() {
+            return isTrue;
+        }
+    }
 }//class
